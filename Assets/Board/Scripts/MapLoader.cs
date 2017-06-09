a9c9ef1892bec85a9ed27a3aa427a75415f68ec7
castellanos70 <castellanos70@gmail.com>
2017-06-09 01:20:36 -0600

    changed map to have only one goal stop

--- Assets/Board/Scripts/MapLoader.cs
+++ Assets/Board/Scripts/MapLoader.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public static class MapLoader : object{
 	                                  
@@ -15,6 +13,10 @@ public static class MapLoader : object{
 	public static GameMap[] loadAllMaps(){
 		
 		Object[] mapTexts = Resources.LoadAll ("Maps", typeof (TextAsset));
+        if (mapTexts.Length == 0)
+        {
+            Debug.Log("ERROR: MapLoader.loadAllMaps(): files not found.");
+        }
 		GameMap[] gameMaps = new GameMap[mapTexts.Length];
 
 		for (int i = 0; i < mapTexts.Length; i++){
