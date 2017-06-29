6fb997e79c89f8b4bc5a3fbc8290b5d30f3a057b
castellanos70 <castellanos70@gmail.com>
2017-06-28 22:20:17 -0600

    removed reading player moves from map files

--- Assets/Board/Scripts/GameMap.cs
+++ Assets/Board/Scripts/GameMap.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 public class GameMap
 {
     private int width, height;
-    private int maxMovesPlayer1 = 0;
-    private int maxMovesPlayer2 = 0;
     private CameraScript.Element [,] map;
 
     public GameMap(string mapString)
@@ -14,15 +12,10 @@ public class GameMap
         
 		string[] lines = mapString.Split(new string[] { "\r\n", "\n" }, System.StringSplitOptions.None);
 
-		string[] movesString = (lines[0]).Split (new string[] { ", " }, System.StringSplitOptions.None);
-
-
-        maxMovesPlayer1 = int.Parse(movesString[0]);
-        maxMovesPlayer2 = int.Parse (movesString [1]);
-		height = lines.Length-1;
+		height = lines.Length;
 		width = 0;
 
-		for (int i = 1; i < lines.Length; i++)
+		for (int i = 0; i < lines.Length; i++)
         {
 			if (lines[i].Length > width) width = lines[i].Length;
 		}
@@ -40,24 +33,18 @@ public class GameMap
 
 		//Debug.Log ("width: " + width + ", height: " + height); 
 
-		for (int j = 1; j < lines.Length; j++)
+		for (int j = 0; j < lines.Length; j++)
         {
 			string row = lines[j];
             //Debug.Log(row+", idx="+ ((lines.Length - 1) - j));
             for (int i = 0; i < row.Length; i++)
             {
                 //Debug.Log(i + "=i" + ",    j="+j + "   width: " + width + ", height: " + height);
-                map[i, (lines.Length - 1) - j] = CameraScript.getElement(row[i]);
+                map[i, (lines.Length -1)- j] = CameraScript.getElement(row[i]);
 			}
 		}
 	}
 
     public CameraScript.Element[,] getMap() { return map; }
 
-    public int getMaxMoves(CameraScript.Element player)
-    {
-        if (player == CameraScript.Element.PLAYER1) return maxMovesPlayer1;
-        if (player == CameraScript.Element.PLAYER2) return maxMovesPlayer2;
-        return 0;
-    }
 }
